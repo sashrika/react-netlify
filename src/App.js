67172840a6afd32cce3f5361a1f817279f7070cf
sashrika <sashrika@liveroom.xyz>
2020-08-03 20:48:22 +0530

    adding envs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My token is {process.env.REACT_APP_MY_TOKEN}
+          My token is {process.env.MY_TOKEN}
         </a>
       </header>
     </div>
